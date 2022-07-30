236ba79dfbb8af7a12cbb33a93d8374a012ca9ec
Marcos Vinícius Bonancin Torrecilha <marcos.torrecilha@gmail.com>
2022-07-29 22:13:19 -0300

    fix calling borrow a book api

--- frontend/src/api/book.js
+++ frontend/src/api/book.js
@@ -12,7 +12,7 @@ export default {
     },
     async borrowBook(id) {
         var user = JSON.parse(localStorage.getItem('loggedUser'));
-        return axios.post(`/api/books/${id}/student/${user.email}?action=borrow`, null,
+        return axios.post(`/api/books/${id}/student/${user.email}`, null,
             { headers: {'Authorization': `Bearer ${user.idToken}`}
         });  
     }
