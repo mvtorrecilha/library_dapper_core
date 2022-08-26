cc924e26e4c2388c77fe76378429866924622080
Marcos Vinícius Bonancin Torrecilha <marcos.torrecilha@gmail.com>
2022-08-26 18:57:52 -0300

    dockerfile sql server configuration

--- README.md
+++ README.md
@@ -34,6 +34,11 @@ This command will run the site on url "https://localhost:5001" and the database 
 
 **Note: Every time you execute this command the data in the tables will be deleted. To not erase the data, just remove the argument "-- seed"**
 
+## Run the tests
+
+```
+dotnet test ./backend/src/Library.UnitTests/Library.UnitTests.csproj
+```
 ## Docker Usage
 
 ```
@@ -41,12 +46,6 @@ docker-compose up
 ```
 
 swagger url: `http://localhost:5000/api/swagger/index.html`
-
-## Run the tests
-
-```
-dotnet test ./backend/src/Library.UnitTests/Library.UnitTests.csproj
-```
 
 ## REST API
 
--- backend/src/Library.Api/Program.cs
+++ backend/src/Library.Api/Program.cs
@@ -1,9 +1,9 @@
 using Library.Repository;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
+using System.IO;
 using System.Linq;
 
 namespace Library.Api;
@@ -11,10 +11,9 @@ namespace Library.Api;
 public class Program
 {
     public static void Main(string[] args)
-    {            
+    {
+        var configs = GetConfiguration();
         var host = CreateHostBuilder(args).Build();
-
-        IConfiguration configs = host.Services.GetService<IConfiguration>();
 
         if (args.Contains("seed"))
         {
@@ -31,4 +30,16 @@ public class Program
             {
                 webBuilder.UseStartup<Startup>();
             });
+
+    private static IConfiguration GetConfiguration()
+    {
+        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+
+        return new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+    }
 }
